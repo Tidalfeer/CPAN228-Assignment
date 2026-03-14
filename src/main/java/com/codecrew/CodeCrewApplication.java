d10cc1c4845a4b05e7c965b965e2f721d3651bcd
Raj Patel <raj.patel514@gmail.com>
2026-03-14 01:24:15 -0400

    Rough layout and css, will need to be changed

---
+++ src/main/java/com/codecrew/CodeCrewApplication.java
@@ -0,0 +1,13 @@
+package com.codecrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCrewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecrew/CodeCrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
