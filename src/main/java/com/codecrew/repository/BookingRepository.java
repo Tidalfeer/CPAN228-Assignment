95b56004c3be0ab61a307a57ec512de3fe1e954c
Ramanpreet Grover <N01698437@HUMBER.CA>
2026-03-15 00:57:08 -0400

    Added query method to BookingRepository for filtering bookings by service type

---
+++ src/main/java/com/codecrew/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.codecrew.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.codecrew.model.Booking;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByServiceType(String serviceType);
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
+VALUES ('Brampton', 'Hardware Support', DATE '2026-03-20', TIME '10:00:00', 'customer1@example.com', CURRENT_TIMESTAMP);
+
+INSERT INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
+VALUES ('Mississauga', 'Software Installation', DATE '2026-03-21', TIME '14:30:00', 'customer2@example.com', CURRENT_TIMESTAMP);
+
+INSERT INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
+VALUES ('Toronto', 'Programming Help', DATE '2026-03-22', TIME '09:15:00', 'customer3@example.com', CURRENT_TIMESTAMP);
