6e979962904a21b1925f963fee46705d45da0091
Tidalfeer <tylerlee1406@gmail.com>
2026-03-15 10:30:08 -0400

    Added BookingRepository.java file to the src/main/java/com/codecrew/repository/ directory.

---
+++ src/main/java/com/codecrew/repository/BookingRepository.java
@@ -0,0 +1,17 @@
+package com.codecrew.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.codecrew.model.Booking;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    Page<Booking> findByServiceType(Long userId, Pageable pageable);
+    Page<Booking> findByServiceDateBetween(LocalDate startDate, LocalDate endDate, Pageable pageable);
+    Page<Booking> findByServiceTypeAndServiceDateBetween(String serviceType, LocalDate startDate, LocalDate endDate, Pageable pageable);
+}
