d7f1f696b70b8d80f194d2c02d3a6ecf10ede9d7
Raj Patel <raj.patel514@gmail.com>
2026-03-29 21:42:04 -0400

    Fixed seeded data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
-VALUES ('Brampton', 'Hardware Support', DATE '2026-03-20', TIME '10:00:00', 'customer1@example.com', CURRENT_TIMESTAMP);
+VALUES ('Brampton', 'Hardware Repair', DATE '2026-06-15', TIME '10:00:00', 'customer1@example.com', CURRENT_TIMESTAMP);
 
 INSERT INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
-VALUES ('Mississauga', 'Software Installation', DATE '2026-03-21', TIME '14:30:00', 'customer2@example.com', CURRENT_TIMESTAMP);
+VALUES ('Mississauga', 'Software Install', DATE '2026-06-16', TIME '14:30:00', 'customer2@example.com', CURRENT_TIMESTAMP);
 
 INSERT INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
-VALUES ('Toronto', 'Programming Help', DATE '2026-03-22', TIME '09:15:00', 'customer3@example.com', CURRENT_TIMESTAMP);
+VALUES ('Toronto', 'Code Review', DATE '2026-06-17', TIME '09:15:00', 'customer3@example.com', CURRENT_TIMESTAMP);
