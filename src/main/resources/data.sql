29a01bf7515aae2147ca09886e73262e1df5d76c
Tidalfeer <tylerlee1406@gmail.com>
2026-04-19 17:09:01 -0400

    corrected error is data.sql that prevented startup.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT IGNORE INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
+INSERT INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
 VALUES ('Brampton', 'Hardware Repair', DATE '2026-06-15', TIME '10:00:00', 'customer1@example.com', CURRENT_TIMESTAMP);
 
-INSERT IGNORE INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
+INSERT INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
 VALUES ('Mississauga', 'Software Install', DATE '2026-06-16', TIME '14:30:00', 'customer2@example.com', CURRENT_TIMESTAMP);
 
-INSERT IGNORE INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
+INSERT INTO booking (location, service_type, service_date, start_time, customer_email, created_at)
 VALUES ('Toronto', 'Code Review', DATE '2026-06-17', TIME '09:15:00', 'customer3@example.com', CURRENT_TIMESTAMP);
